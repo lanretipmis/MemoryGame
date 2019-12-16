c2f6e06bd4a7255e57ea8abdece9231d472f671a
lanretipmis <54453873+lanretipmis@users.noreply.github.com>
2019-12-16 21:57:30 +0300

    some js

--- src/_deck.js
+++ src/_deck.js
@@ -1,6 +1,30 @@
-export class Deck {
+class Deck {
     constructor (){
+        this.cardDeck = [
+            {icon : '<i class="fas fa-frog"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-frog"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-atom"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-atom"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-bath"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-bath"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-bowling-ball"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-bowling-ball"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-brain"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-brain"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fab fa-forumbee"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fab fa-forumbee"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-gem"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-gem"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-guitar"></i>', faceUp : false, matched: false},
+            {icon : '<i class="fas fa-guitar"></i>', faceUp : false, matched: false},
+        ]
+    }
 
+    shuffle (){
+        let shuffled = this.cardDeck.sort(()=>{
+            return Math.random() - 0.5;
+        })
+        console.log(shuffled);
     }
 }
 
